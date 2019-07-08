0e74663227b2f825bd9db5d9294619f4a756a981
Surat Teerapittayanon <steerapi@gmail.com>
2019-07-08 11:35:33 +0700

    bump version

--- bump-version.js
+++ bump-version.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const version = require('./package.json').version;
-
+console.log(version)
 let data = fs.readFileSync('./plugin.xml', "utf8");
-data = data.replace(/plugin id="cordova-plugin-firebase" version="[^"]+"/, `plugin id="cordova-plugin-firebase" version="${version}"`);
+data = data.replace(/plugin id="cordova-plugin-firebasex" version="[^"]+"/, `plugin id="cordova-plugin-firebasex" version="${version}"`);
 fs.writeFileSync('./plugin.xml', data);
