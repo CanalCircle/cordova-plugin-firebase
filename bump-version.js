02b3bdc41de433ee9a9b8d6b3eb472c188a1b220
Surat Teerapittayanon <steerapi@gmail.com>
2019-07-08 12:01:25 +0700

    Update name

--- bump-version.js
+++ bump-version.js
@@ -4,5 +4,5 @@ const fs = require('fs');
 const version = require('./package.json').version;
 console.log(version)
 let data = fs.readFileSync('./plugin.xml', "utf8");
-data = data.replace(/plugin id="@canalcircle\/cordova-plugin-firebasex" version="[^"]+"/, `plugin id="@canalcircle/cordova-plugin-firebasex" version="${version}"`);
+data = data.replace(/plugin id="@canalcircle\/cordova-plugin-firebasex-cc" version="[^"]+"/, `plugin id="@canalcircle/cordova-plugin-firebasex-cc" version="${version}"`);
 fs.writeFileSync('./plugin.xml', data);
